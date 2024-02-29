054f2d788e1789361df1922932bdfe1f9f0d7eec
Thamires Machado Barrozo - 823114039 <823114039@ulife.com.br>
2024-02-28 21:18:39 -0300

    Primeiro teste concluido com sucesso

--- Empregado.java
+++ Empregado.java
@@ -31,10 +31,29 @@ public class Empregado{
     this.idade = idade;
     }
 
-        public void setTipo (int tipo){
+    public void setTipo (int tipo){
     this.tipo = tipo;
     }
     public int getTipo (){
     return this.tipo;
+    }
+
+    public void setSalario (double salario){
+    this.salario = salario;
+    }
+    public double getSalario (){
+    return this.salario;
+    }
+    public void setComissao (double comissao){
+    this.comissao = comissao;
+    }
+    public double getComissao (){
+    return this.comissao;
+    }
+    public void setBonus (double bonus){
+    this.bonus = bonus;
+    }
+    public double getBonus (){
+    return this.bonus;
     }
 }
